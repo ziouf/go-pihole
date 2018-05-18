c60a49e2c3b0cd0d55f3b059852852819878a451
Cyril <marin.cyril@gmail.com>
2018-05-18 14:54:17 +0200

    [Upt] Use Admin-LTE as UI base template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,55 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Resource from 'vue-resource'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+import routes from './routes'
+import store from './store'
 
-/* eslint-disable no-new */
+// Resource logic
+Vue.use(Resource)
+Vue.http.options.emulateJSON = true
+
+Vue.use(VueRouter)
+
+// Import top level component
+import App from './App.vue'
+
+// Routing logic
+var router = new VueRouter({
+  routes: routes,
+  mode: 'history',
+  scrollBehavior: function (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
+})
+
+// Check local storage to handle refreshes
+if (window.localStorage) {
+  if (store.state.token !== window.localStorage.getItem('token')) {
+    store.commit('SET_TOKEN', window.localStorage.getItem('token'))
+  }
+}
+
+// Some middleware to help us ensure the user is authenticated.
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth) && (store.state.token === null)) {
+    next('/login')
+  } else if ((store.state.token !== null) && to.path === '/login') {
+    next('/dashboard')
+  } else {
+    next()
+  }
+})
+
+// Start out app!
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  router: router,
+  store: store,
+  render: h => h(App)
 })
+
+require('bootstrap')
+require('admin-lte')
+require('../node_modules/admin-lte/dist/js/demo.js')
